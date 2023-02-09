1b8113f965626d9f52a70494de1d8565149dd338
TrueVale <srvleh@gmail.com>
2023-02-09 11:01:04 +0100

    [DEV-0011] Layout links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,6 +38,6 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    redirect_to article_path
+    redirect_to articles_path
   end
 end

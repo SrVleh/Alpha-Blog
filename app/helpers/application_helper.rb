2278345e665f1832ac51f5416d45f19816ea960e
TrueVale <srvleh@gmail.com>
2023-02-14 23:39:40 +0100

    [DEV-0032] Add users index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
     hash = Digest::MD5.hexdigest(email_address)
     size = otions[:size]
     gravatar_url = "https://www.gravatar.com/avatar/#{ hash }?s=#{ size }"
-    image_tag(gravatar_url, alt: user.username, class: "rounded md-auto d-block mt-4")
+    image_tag(gravatar_url, alt: user.username, class: "rounded md-auto d-block mt-4 w-25")
   end
 end
